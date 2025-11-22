a82b9989b76a303757c8c0f8da0d1caf67df4ba5
Pratiksha <pratiksha23shukla@gmail.com>
2025-11-22 09:55:14 +0530

    react js first app

---
+++ first/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import JSXDemo from './JSXDemo';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        /*calling components<ComponentName/>*/}
+        <JSXDemo/>
+    </div>
+  );
+}
---
+++ first/src/JSXDemo.jsx
@@ -0,0 +1,21 @@
+function jsxDemo(){
+    return(
+        <div>
+        <h1>JSX Example</h1>
+        </div>
+    )
+}
+export default JSXDemo;
+
+
+
+
+
+
+
+
+
+
+
+
+
